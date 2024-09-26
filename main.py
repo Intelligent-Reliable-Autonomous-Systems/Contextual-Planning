97fa851c8678a7f74f4d8db0e3ecd2106fbf9a49
Your Name <you@example.com>
2024-09-26 09:44:46 -0700

    added single preference scaparized baseline

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import oracle_policy
 import metareasoner as MR
 from timeit import default_timer as timer
 
-domain = 'taxi' # options - ['salp', 'warehouse', 'taxi']
-context_sim = 1 # options - [0, 1, 2, 3, 4, 5, 6]
+domain = 'warehouse' # options - ['salp', 'warehouse', 'taxi']
+# context_sim = 1 # options - [0, 1, 2, 3, 4, 5, 6]
 
-for context_sim in range(6):
+for context_sim in range(7):
     if domain == 'salp':
         from salp_mdp import SalpEnvironment, SalpAgent
         Env = SalpEnvironment("grids/salp/illustration_eddy.txt", context_sim)
@@ -27,15 +27,13 @@ for context_sim in range(6):
     savenames = {0: 'Task only',
                 1: 'Task > NSE Mitigation', 
                 2: 'NSE Mitigation > Task', 
-                3: 'Scalarization',
-                4: 'Contextual Approach without Conflict Resolution', 
-                5: 'Contextual Approach with Conflict Resolution',
-                6: 'Oracle'}
+                3: 'Scalarization Single Preference',
+                4: 'Scalarization Contextual Preferences',
+                5: 'Contextual Approach without Conflict Resolution', 
+                6: 'Contextual Approach with Conflict Resolution'}
 
     print(simple_colors.cyan('Context Simulation: ' + savenames[context_sim], ['bold', 'underlined']))
-    if context_sim == 6:
-        Pi_G = oracle_policy.contextual_lexicographic_value_iteration_oracle(agent)
-    elif context_sim == 3:
+    if context_sim in [3, 4]:
         agent, Pi_G = value_iteration.contextual_scalarized_value_iteration(agent)
     else:
         agent, Pi_G = value_iteration.contextual_lexicographic_value_iteration(agent)
@@ -43,7 +41,7 @@ for context_sim in range(6):
     conflict = MR.conflict_checker(Pi_G, agent)
     if conflict: print(simple_colors.red('Conflict Detected!', ['bold']) )
     else: print(simple_colors.green('No Conflicts', ['bold']))
-    if context_sim == 5:
+    if context_sim == 6:
         Pi_G = MR.conflict_resolver(Pi_G, agent)
         conflict = MR.conflict_checker(Pi_G, agent)
         if conflict: print(simple_colors.red('Conflict Detected!', ['bold']) )
